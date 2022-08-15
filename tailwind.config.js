fc8fd35cd06e5fd7c11b09bce87e67ac35072f80
coditis <coditis@github.com>
2022-08-15 17:39:45 +0300

    Updated package structura and added custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     content: ["./index.html"],
     theme: {
         extend: {
-
+            colors: {
+                // 'color-name': '#XXXXXX',
+            },
+            fontFamily: {
+                // serif: ['FontName', ...defaultTheme.fontFamily.serif],
+                // sans: ['FontName', ...defaultTheme.fontFamily.sans],
+				// custom-property: ['Font', 'sans']
+            }
         },
     },
     plugins: [],
 };
+
+// Uncomment the lines to add custom colors or custom self-hosted fonts
